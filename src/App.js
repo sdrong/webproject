f42493b5715953debd853e9a8611a90d2bfdb8dd
sdrong <101074310+sdrong@users.noreply.github.com>
2023-06-11 16:21:47 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -24,8 +24,8 @@ import LoginPage from "./component/page/LoginPage";
 import SignupPage from "./component/page/SignupPage";
 
 const MainTitleBox = styled.div`
-    background-color: limegreen;
-    padding: 10px;
+  background-color: gray;
+  padding: 10px;
 `;
 
 const MainTitleText = styled(NavLink)`
@@ -45,7 +45,16 @@ const CustomTitleText = styled(MainTitleText)`
 const GlobalStyle = createGlobalStyle`
   body {
     background-color: #f1f1f1;
+    margin: 0;
+    padding: 0;
   }
+`;
+
+
+const AppContainer = styled.div`
+  max-width: 800px;
+  margin: 0 auto;
+  padding: 20px;
 `;
 
 function App(props) {
@@ -60,28 +69,33 @@ function App(props) {
   
   // // // spring과 통신하는 부분
 
-  return (
+   return (
+    <>
+      <GlobalStyle />
       <BrowserRouter>
-      <MainTitleBox>
-        <MainTitleText>문제만들기 사이트</MainTitleText>
-      </MainTitleBox>
-      <Routes>
-        <Route index element={<MainPage />} />
-        <Route path="/categories" element={<MainPage />} />
-        <Route path="/categories/:mainId/problems" element={<PostPage />} />
-        <Route path="/post-write1/:writeId" element={<PostWritePage1 />} />
-        <Route path="/post-write2/:writeId" element={<PostWritePage2 />} />
-        <Route path="/post-write3/:writeId" element={<PostWritePage3 />} />
-        <Route path="/problems/:problemId" element={<PostViewPage />} />
-        <Route path="/problems2/:problemId" element={<PostViewPage2 />} />
-        <Route path="/problems3/:problemId" element={<PostViewPage3 />} />
-        <Route path="/problems/:problemId/comments" element={<CommentPage />} />
-        <Route path="/comments/:commentId" element = {<UpdateCommentPage/>}/>
-        <Route path="/login" element = {<LoginPage/>}/>
+        <MainTitleBox className="bg-dark text-white p-2">
+          <MainTitleText to="/">문제만들기 사이트</MainTitleText>
+        </MainTitleBox>
+        <AppContainer className="bg-light p-4">
+          <Routes>
+            <Route index element={<MainPage />} />
+            <Route path="/categories" element={<MainPage />} />
+            <Route path="/categories/:mainId/problems" element={<PostPage />} />
+            <Route path="/post-write1/:writeId" element={<PostWritePage1 />} />
+            <Route path="/post-write2/:writeId" element={<PostWritePage2 />} />
+            <Route path="/post-write3/:writeId" element={<PostWritePage3 />} />
+            <Route path="/problems/:problemId" element={<PostViewPage />} />
+            <Route path="/problems2/:problemId" element={<PostViewPage2 />} />
+            <Route path="/problems3/:problemId" element={<PostViewPage3 />} />
+            <Route path="/problems/:problemId/comments" element={<CommentPage />} />
+            <Route path="/comments/:commentId" element={<Update_CommentPage />} />
+            <Route path="/login" element = {<LoginPage/>}/>
         <Route path="/signup" element = {<SignupPage/>}/>
-      </Routes>
-    </BrowserRouter>
-      );
+          </Routes>
+        </AppContainer>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
